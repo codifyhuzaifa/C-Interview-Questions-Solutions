4c73836c4eb6127943fbe9563ee8999abea7598c
Huzaifa Farooq <huzaifa1947@gmail.com>
2025-02-10 22:35:02 +0500

    Check Vowel or Consonant

---
+++ check_vowel_consonant.cpp
@@ -0,0 +1,21 @@
+// C++ Practice Question for Interviews
+// Write a Program to Print Check Whether a Character is an Alphabet or Not
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   char check;
+   
+   cout << "Enter an alphabet to check whether it's Vowel or Consonant: ";
+   cin >> check;
+   
+   if( (check >= 'A' && check <= 'Z') || (check >= 'a' && check <= 'z')) {
+    cout << "It's an alphabet" << endl;
+    }
+    else {
+    cout << "It's not an alphabet" << endl;
+    }
+
+   return 0;
+}
